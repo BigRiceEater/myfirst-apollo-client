c096f8406c831b8007c3098591776d31d73155b2
BigRiceEater <34292181+BigRiceEater@users.noreply.github.com>
2019-05-02 18:08:00 +0800

    Add apollo client

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import {ApolloProvider} from "react-apollo";
+import ApolloClient from "apollo-boost";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000"
+});
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+      Apollo client with React!
+      </div>
+    </ApolloProvider>
   );
 }
 
